8051255d83674857d6b835a256dcc5cba68c00ea
kimsehun0530 <kimsehun0530@gmail.com>
2025-09-30 21:25:41 +0900

    3
    3

--- Assets/C/S.cs
+++ Assets/C/S.cs
@@ -7,10 +7,40 @@ public class S : MonoBehaviour
     [SerializeField] float speed = 10f;
     [SerializeField] float xRange = 5f;
     [SerializeField] float yRange = 5f;
+
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -10f;
+    [SerializeField] float postionYawFactor = 2f;
+    [SerializeField] float controlYawFactor = -10f;
+    [SerializeField] float controlRollFactor = 5f;
+
+    float hor;
+    float ver;
+    
      void Update()
+     {
+        ProcessTranslation();
+        ProcessRotation();
+     }
+
+     void ProcessRotation()
+     {
+        float pitchPos = transform.localPosition.y * positionPitchFactor;
+        float pitchCon = ver * controlPitchFactor;
+        float pitch = pitchPos + pitchCon;
+
+        float yawPos = transform.localPosition.x * postionYawFactor;
+        float yawCon = hor * controlPitchFactor;
+        float yaw = yawPos + yawCon;
+
+        float roll = hor * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+     }
+     private void ProcessTranslation()
     {
-        float hor = Input.GetAxis("Horizontal");
-        float ver = Input.GetAxis("Vertical");
+        hor = Input.GetAxis("Horizontal");
+        ver = Input.GetAxis("Vertical");
 
         float xDffset = hor * Time.deltaTime * speed;
         float newXpos = transform.localPosition.x + xDffset;
